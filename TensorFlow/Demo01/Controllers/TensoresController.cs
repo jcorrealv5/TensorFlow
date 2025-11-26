03cafa13fe38acbad86e21ec4399f397e88cd550
jhon correal <jhonfresan@hotmail.com>
2025-11-25 23:52:37 -0500

    inicio curso TF

---
+++ TensorFlow/Demo01/Controllers/TensoresController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Demo01.Controllers
+{
+    public class TensoresController : Controller
+    {
+        // GET: Tensores
+        public ActionResult Operaciones()
+        {
+            return View();
+        }
+    }
+}
---
+++ TensorFlow/Demo01/Scripts/Operaciones.js
@@ -0,0 +1,77 @@
+﻿var c = 0;
+var rpta = {};
+var tensor1, tensor2;
+
+window.onload = function () {
+    btnEscalares.onclick = function () {
+        var escalarTexto = tf.scalar("TensorFlow JS");
+        var escalarDecimal = tf.scalar(3.2);
+        escalarTexto.print();
+        escalarDecimal.print();
+        rpta = {};
+        c = 0;
+        escalarTexto.data().then(data => {
+            rpta["Tensor Escalar Texto"] = data;
+            c++;
+            mostrarRespuesta(2);
+        });
+        escalarDecimal.data().then(data => {
+            rpta["Tensor Escalar Decimal"] = data;
+            c++;
+            mostrarRespuesta(2);
+        });
+    }
+
+    btnTensores.onclick = function () {
+        tensor1 = tf.tensor([1, 2, 3]);
+        tensor2 = tf.tensor([4, 5, 6]);
+        rpta = {};
+        c = 0;
+        tensor1.data().then(data => {
+            rpta["Tensor 1"] = data;
+            c++;
+            mostrarRespuesta(2);
+        });
+        tensor2.data().then(data => {
+            rpta["Tensor 2"] = data;
+            c++;
+            mostrarRespuesta(2);
+        });
+    }
+
+    btnSumar.onclick = function () {
+        var suma = tensor1.add(tensor2);
+        suma.data().then(data => {
+            rpta["Suma"] = data;
+            c++;
+            mostrarRespuesta(3);
+        });
+    }
+
+    btnMultiplicacion.onclick = function () {
+        var producto = tensor1.mul(tensor2);
+        producto.data().then(data => {
+            rpta["Producto"] = data;
+            c++;
+            mostrarRespuesta(3);
+        });
+    }
+
+    btnProductoPunto.onclick = function () {
+        var productoPunto = tensor1.dot(tensor2);
+        productoPunto.data().then(data => {
+            rpta["Producto Punto"] = data;
+            c++;
+            mostrarRespuesta(3);
+        });
+    }
+}
+
+function mostrarRespuesta(x) {
+    divRpta.innerHTML = "";
+    if (c == x) {
+        for (clave in rpta) {
+            divRpta.innerHTML += clave + " = " + rpta[clave] + "<br>";
+        }        
+    }
+}
